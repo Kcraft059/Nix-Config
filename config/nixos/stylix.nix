05d96ff99e759937dcd30de29d40b52d828476a7
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-21 15:53:22 +0200

    fix(stylix)

--- config/nixos/stylix.nix
+++ config/nixos/stylix.nix
@@ -9,7 +9,7 @@
     nixos-system.stylix.enable = lib.mkEnableOption ''Desktop manager wide theme'';
   };
 
-  config = {
+  config = lib.mkIf nixos-system.stylix.enable {
     stylix.enable = true;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
     stylix.image = ../../ressources/ign_colorful.png;
