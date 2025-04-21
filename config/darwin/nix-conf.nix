552703d4dbe96fb8cb1fcffbb44b9f6812a6c486
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-21 11:37:42 +0200

    fix(nix darwin nix-conf interval)

--- config/darwin/nix-conf.nix
+++ config/darwin/nix-conf.nix
@@ -19,42 +19,19 @@
     # Auto upgrade nix package and the daemon service.
     # services.nix-daemon.enable = true;
     # nix.package = pkgs.nix;
-    nix = {
-      enable = true;
-      settings = {
-        experimental-features = "nix-command flakes";
-      };
-      extraOptions = lib.optionalString (pkgs.system == "aarch64-darwin") ''
-        extra-platforms = x86_64-darwin aarch64-darwin
-      '';
-      optimise = lib.mkIf config.nix-conf.garbage-collect.enable {
-        automatic = true;
-        interval = {
-          #Weekday = 1;
-          Hour = 6;
-          #Minute = 0;
-        };
-      };
-      gc = lib.mkIf config.nix-conf.garbage-collect.enable {
-        automatic = true;
-        interval = {
-          #Weekday = 1;
-          Hour = 6;
-          #Minute = 0;
-        };
-        options = "-d --delete-older-than 15d";
-      };
+
+    nix.optimise.interval = lib.mkIf config.nix-conf.garbage-collect.enable {
+      #Weekday = 1;
+      Hour = 6;
+      #Minute = 0;
     };
 
-    nixpkgs.hostPlatform = system; # Already specified dynamically
-    nixpkgs.config.allowUnfree = true;
-    #nixpkgs.config.allowUnsupportedSystem = true;
-    #nixpkgs.config.allowBroken = true;
+    nix.gc.interval = lib.mkIf config.nix-conf.garbage-collect.enable {
+      #Weekday = 1;
+      Hour = 6;
+      #Minute = 0;
+    };
 
-    # Create /etc/zshrc that loads the nix-darwin environment.
-    # programs.zsh.enable = true;
-    # Set Git commit hash for darwin-version.
-    system.configurationRevision = self.rev or self.dirtyRev or null;
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
     system.stateVersion = 5;
