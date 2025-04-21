235371d32a3df9aec9ea31810c41ed2853ef6508
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-21 11:26:31 +0200

    fix(nix.gc)

---
+++ config/common/nix-conf.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  config,
+  self,
+  lib,
+  system,
+  ...
+}:
+{
+  options.nix-conf = {
+    garbage-collect.enable = lib.mkEnableOption "Whether to enable GC & OPTIMISE periodically";
+  };
+
+  config = {
+    # Auto upgrade nix package and the daemon service.
+    # services.nix-daemon.enable = true;
+    # nix.package = pkgs.nix;
+    nix.enable = true;
+    nix.settings = {
+        experimental-features = "nix-command flakes";
+      };
+    nix.extraOptions = lib.optionalString (pkgs.system == "aarch64-darwin") ''
+        extra-platforms = x86_64-darwin aarch64-darwin
+      '';
+    nix.optimise.automatic = lib.optionals config.nix-conf.garbage-collect.enable true;
+    nix.gc.automatic = lib.optionals config.nix-conf.garbage-collect.enable true; 
+    nix.gc.options = lib.optionals config.nix-conf.garbage-collect.enable "-d --delete-older-than 15d"; 
+
+    nixpkgs.hostPlatform = system; # Already specified dynamically
+    nixpkgs.config.allowUnfree = true;
+    #nixpkgs.config.allowUnsupportedSystem = true;
+    #nixpkgs.config.allowBroken = true;
+
+    # Create /etc/zshrc that loads the nix-darwin environment.
+    # programs.zsh.enable = true;
+    # Set Git commit hash for darwin-version.
+    system.configurationRevision = self.rev or self.dirtyRev or null;
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 5;
+  };
+}
---
+++ config/nixos/nix-conf.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  config,
+  self,
+  lib,
+  system,
+  ...
+}:
+{
+  options.nix-conf = {
+    garbage-collect.enable = lib.mkEnableOption "Whether to enable GC & OPTIMISE periodically";
+  };
+
+  import = [
+    ./../common/nix-conf.nix
+  ];
+
+  config = {
+    nix.optimise.dates = lib.optionals config.nix-conf.garbage-collect.enable "12hr";
+    nix.gc.dates = lib.optionals config.nix-conf.garbage-collect.enable "12hr";
+  };
+}
