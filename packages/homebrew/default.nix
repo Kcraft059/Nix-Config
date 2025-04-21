957d8ca91d9e6057fcd5ac5a8ae0aed86491b902
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-21 11:08:44 +0200

    fix(separated homebrew and nix packages)

--- packages/homebrew/default.nix
+++ packages/homebrew/default.nix
@@ -2,10 +2,7 @@
 {
     imports = [
         ./homebrew.nix
-        ./nixpackages.nix
     ];
-    NIXPKG.GUIapps.enable = lib.mkDefault true;
-    NIXPKG.darwinApps.enable = lib.mkDefault false;
     HMB.enable = lib.mkDefault true;
     HMB.casks.enable = lib.mkDefault true;
     HMB.brews.enable = lib.mkDefault true;
