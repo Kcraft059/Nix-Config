a45c3eeb6c850ac009e85c18dfe82fa8e9b9b7a3
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-23 19:29:29 +0200

    test

--- home/nixos/hyprland.nix
+++ home/nixos/hyprland.nix
@@ -9,39 +9,61 @@
     hyprland.enable = lib.mkEnableOption ''Enable Hyprland and its plugins'';
   };
 
-  config = lib.mkIf config.hyprland.enable {
+  config =
+    let
+      startupScript = pkgs.pkgs.writteShellScriptBin "start" ''
+        ${pkgs.waybar}/bin/waybar &
+        ${pkgs.swww}/bin/swww-daemon & 
 
-    # HyprLand
+        sleep 1
 
-    wayland.windowManager.hyprland = {
-      enable = true;
-    };
+        ${pkgs.swww}/bin/swww img ${../../ressources/ign_colorful.png};
 
-    # Hyprpaper
-    services.hyprpaper = {
-      enable = true;
-      settings = {
-        preload = [ "/home/camille/.config/hypr/ign_colorful.png" ];
-        wallpaper = [ "monitor,/home/camille/.config/hypr/ign_colorful.png" ];
+      '';
+
+    in
+    lib.mkIf config.hyprland.enable {
+
+      # HyprLand
+
+      wayland.windowManager.hyprland = {
+        enable = true;
+        settings = {
+          exec-once = ''${startupScript}/bin/start'';
+        };
+      };
+
+      # Hyprpaper
+
+      home.packages = [
+        swww
+      ];
+      /*
+        services.hyprpaper = {
+          enable = true;
+          settings = {
+            preload = [ "/home/camille/.config/hypr/ign_colorful.png" ];
+            wallpaper = [ "monitor,/home/camille/.config/hypr/ign_colorful.png" ];
+          };
+        };
+      */
+
+      home.file.".config/hypr/ign_coloful.png".source = ../../ressources/ign_colorful.png;
+
+      # Plugins / gui programs/menus
+
+      programs.waybar = {
+        enable = true;
+        package = pkgs.waybar.overrideAttrs (oldAttrs: {
+          mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        });
+      };
+      programs.thunar = {
+        enable = true;
+        plugins = with pkgs.xfce; [
+          thunar-archive-plugin
+          thunar-volman
+        ];
       };
     };
-
-    home.file.".config/hypr/ign_coloful.png".source = ../../ressources/ign_colorful.png;
-
-    # Plugins / gui programs/menus
-
-    programs.waybar = {
-      enable = true;
-      package = pkgs.waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-      });
-    };
-    programs.thunar = {
-      enable = true;
-      plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-volman
-      ];
-    };
-  };
 }
