826728dd58ed0613c0569ff82544b1a95ccd121a
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-23 19:59:03 +0200

    update (hyprland)

--- home/nixos/hyprland.nix
+++ home/nixos/hyprland.nix
@@ -30,8 +30,76 @@
         enable = true;
         settings = {
           exec-once = ''${startupScript}/bin/start'';
+          exec-once = "$terminal";
+          "$terminal" = ghostty;
+          "$fileManager" = thunar;
+          source = "~/.config/hypr/rose-pine-moon.conf";
+          general = {
+            gaps_in = 5;
+            gaps_out = 15;
+            border_size = 2;
+            col.active_border = "$pine $iris 45deg";
+            ol.inactive_border = "$muted";
+            resize_on_border = true;
+            layout = "dwindle";
+          };
+          decoration = {
+            rounding = 10;
+            rounding_power = 2;
+            active_opacity = 0.90;
+            inactive_opacity = 0.85;
+            shadow = {
+              enabled = true;
+              range = 4;
+              render_power = 5;
+              color = "rgba(1a1a1aee)";
+            };
+            blur = {
+              enabled = true;
+              size = 7;
+              passes = 3;
+              vibrancy = 0.1696;
+            };
+            dwindle = {
+              pseudotile = true;
+              preserve_split = true;
+            };
+            input = {
+              kb_layout = "fr";
+              follow_mouse = 1;
+              touchpad = {
+                natural_scroll = false;
+              };
+            };
+            gestures = {
+              workspace_swipe = true;
+            };
+            "$mainMod" = SUPER;
+            bind = "$mainMod, Q, exec, $terminal";
+            bind = "$mainMod, C, killactive,";
+            bind = "$mainMod, M, exit,";
+          };
         };
       };
+
+      home.file.".config/hypr/rose-pine-moon.conf".text = ''
+        $base	        = 0xff232136
+        $surface        = 0xff2a273f
+        $overlay        = 0xff393552
+        $muted          = 0xff6e6a86
+        $subtle         = 0xff908caa
+        $text           = 0xffe0def4
+        $love           = 0xffeb6f92
+        $gold           = 0xfff6c177
+        $rose           = 0xffea9a97
+        $pine           = 0xff3e8fb0
+        $foam           = 0xff9ccfd8
+        $iris           = 0xffc4a7e7
+        $highlightLow   = 0xff2a283e
+        $highlightMed   = 0xff44415a
+        $highlightHigh  = 0xff56526e
+      '';
+      home.file.".config/hypr/ign_coloful.png".source = ../../ressources/ign_colorful.png;
 
       # Hyprpaper
 
@@ -48,8 +116,6 @@
         };
       */
 
-      home.file.".config/hypr/ign_coloful.png".source = ../../ressources/ign_colorful.png;
-
       # Plugins / gui programs/menus
 
       programs.waybar = {
@@ -58,12 +124,14 @@
           mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
         });
       };
-      /* programs.thunar = {
-        enable = true;
-        plugins = with pkgs.xfce; [
-          thunar-archive-plugin
-          thunar-volman
-        ];
-      }; */
+      /*
+        programs.thunar = {
+          enable = true;
+          plugins = with pkgs.xfce; [
+            thunar-archive-plugin
+            thunar-volman
+          ];
+        };
+      */
     };
 }
