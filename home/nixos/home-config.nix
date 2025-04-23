63557d72547482fdb21bfa371383f7d1d9c76152
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-23 19:40:10 +0200

    i love doing fixes

--- home/nixos/home-config.nix
+++ home/nixos/home-config.nix
@@ -7,7 +7,6 @@
 {
   options.home-config = {
     GUIapps.enable = lib.mkEnableOption "Install GUI-Apps ?";
-    hyprland.enable = lib.mkEnableOption "Configure and use hyprland";
   };
 
   imports = [
