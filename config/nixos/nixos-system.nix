03635d5f1f1012210b794efb9bf63229a00b5e15
Kcraft059 <kcraft059.msg@gmail.com>
2025-04-25 18:29:06 +0200

    Added Wlogout

--- config/nixos/nixos-system.nix
+++ config/nixos/nixos-system.nix
@@ -53,7 +53,7 @@
 
     # Enable the X11 windowing system.
     # You can disable this if you're only using the Wayland session.
-    services.xserver.enable = config.nixos-system.plasma6.enable;
+    services.xserver.enable = true;
 
     # Enable the KDE Plasma Desktop Environment.
     services.displayManager.sddm.enable = config.nixos-system.plasma6.enable;
