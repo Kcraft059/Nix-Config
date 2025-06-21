84fe4bc88bd3a625ab8b086ba69e7174d1816cc2
Kcraft059 <kcraft059.msg@gmail.com>
2025-06-21 13:33:39 +0200

    Added SSH home-config

---
+++ home/darwin/ssh.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  programs.ssh.enable = true;
+  programs.ssh.matchBlocks = {
+    FTN-Server = {
+      port = 22;
+      hostname = "ftnetwork.duckdns.org";
+      user = "server";
+      identityFile = builtins.readFile(./ressources/credentials);
+    };
+  };
+}
