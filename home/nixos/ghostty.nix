b5dc954a279065fe6967cea670f49f66babd92be
Kcraft059 <kcraft059.msg@gmail.com>
2025-07-06 15:36:40 +0200

    using home-manager declaration for ghostty on nixos

--- home/nixos/ghostty.nix
+++ home/nixos/ghostty.nix
@@ -1,25 +1,27 @@
-{ pkgs, lib, ... }:
-let
-  inherit (lib.generators) toKeyValue mkKeyValueDefault;
-in
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   # home.packages = [ pkgs.ghostty ];
-  home.file.".config/ghostty/config".text = toKeyValue { mkKeyValue = mkKeyValueDefault { } " = "; } {
-    theme = "rose-pine-moon";
-    background-blur = 15;
-    background-opacity = 0.85;
-    window-step-resize = true;
-    window-height = 30;
-    window-width = 109;
-    window-padding-x = 5;
-    quit-after-last-window-closed = true;
-    macos-icon = "custom-style";
-    macos-icon-ghost-color = "#F6C177";
-    macos-icon-screen-color = "#232137";
-    #keybind = global:cmd+i=change_title_prompt
-    keybind = "global:cmd+opt+ctrl+space=toggle_quick_terminal";
-    quick-terminal-position = "center";
-    auto-update = "download";
-    term = "xterm-256color";
+  programs.ghostty = {
+    enable = config.home-config.GUIapps.enable;
+    settings = {
+      theme = lib.mkForce "rose-pine-moon";
+      background-blur = 15;
+      background-opacity = 0.85;
+      window-step-resize = true;
+      window-height = 30;
+      window-width = 109;
+      window-padding-x = 5;
+      quit-after-last-window-closed = true;
+      #keybind = global:cmd+i=change_title_prompt
+      #keybind = [ "global:cmd+opt+ctrl+space=toggle_quick_terminal" ];
+      quick-terminal-position = "center";
+      auto-update = "download";
+      term = "xterm-256color";
+    };
   };
 }
