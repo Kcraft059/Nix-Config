f137c714ea6773c2c82828a0e4d6b50e53926f0c
Kcraft059 <kcraft059.msg@gmail.com>
2025-09-06 12:33:58 +0200

    added DockDoor plist

--- home/darwin/app-defaults.nix
+++ home/darwin/app-defaults.nix
@@ -10,5 +10,6 @@
       (name: value: lib.nameValuePair ("/Users/camille/Library/Preferences/" + name) (value))
       {
         "com.apple.screencapture.plist".source = ../configs/defaults-plists/com.apple.screencapture.plist;
+        "com.ethanbills.DockDoor.plist".source = ../configs/defaults-plists/com.ethanbills.DockDoor.plist;
       };
 }
