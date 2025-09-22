bbf003d0ac1fecca02fe9e74c8b336694f4686c8
Kcraft059 <kcraft059.msg@gmail.com>
2025-09-22 11:27:11 +0200

    Modified VsCode font with conditionnal import

--- home/vscode.nix
+++ home/vscode.nix
@@ -2,29 +2,40 @@
   pkgs,
   config,
   lib,
+  systemFonts,
   ...
 }:
 {
   programs.vscode = {
     package = pkgs.vscode;
-    enable = config.home-config.GUIapps.enable ;
+    enable = config.home-config.GUIapps.enable;
     profiles.default = {
-      userSettings = {
-        "workbench.colorTheme" = lib.mkForce "Rosé Pine Moon";
-        "editor.fontSize" = lib.mkForce 13.0;
-        "debug.console.fontSize" = lib.mkForce 13.0;
-        "markdown.preview.fontSize" = lib.mkForce 13.0;
-        "scm.inputFontSize" = lib.mkForce 13.0;
-        "terminal.integrated.fontSize" = lib.mkForce 13.0;
-        "git.openRepositoryInParentFolders" = "always";
-        "explorer.confirmDragAndDrop" = false;
-        "git.confirmSync" = false;
-        "explorer.confirmDelete" = false;
-        "editor.detectIndentation" = false;
-        "editor.tabSize" = 2;
-        "git.suggestSmartCommit" = true;
-        "shfmt.executablePath" = "${pkgs.shfmt}/bin/shfmt";
-      };
+      userSettings =
+        let
+          font = lib.optionalString (builtins.elem pkgs.nerd-fonts.jetbrains-mono systemFonts) lib.mkForce "JetBrainsMono Nerd Font";
+        in
+        {
+          "workbench.colorTheme" = lib.mkForce "Rosé Pine Moon";
+          "editor.fontSize" = lib.mkForce 13.0;
+          "debug.console.fontSize" = lib.mkForce 13.0;
+          "markdown.preview.fontSize" = lib.mkForce 13.0;
+          "scm.inputFontSize" = lib.mkForce 13.0;
+          "terminal.integrated.fontSize" = lib.mkForce 13.0;
+          "git.openRepositoryInParentFolders" = "always";
+          "explorer.confirmDragAndDrop" = false;
+          "git.confirmSync" = false;
+          "explorer.confirmDelete" = false;
+          "editor.detectIndentation" = false;
+          "editor.tabSize" = 2;
+          "git.suggestSmartCommit" = true;
+          "shfmt.executablePath" = "${pkgs.shfmt}/bin/shfmt";
+          "editor.fontFamily" = font;
+          "debug.console.fontFamily" = font;
+          "chat.editor.fontFamily" = font;
+          "scm.inputFontFamily" = font;
+          "editor.inlayHints.fontFamily" = font;
+          "editor.inlineSuggest.fontFamily" = font;
+        };
       extensions = with pkgs.vscode-marketplace; [
         # To use, needs to overlay inputs.nix-vscode-extensions.overlays.default
         jnoortheen.nix-ide
@@ -32,7 +43,7 @@
         bmewburn.vscode-intelephense-client
         mkhl.shfmt
         dnicolson.binary-plist
-        /* SPGoding.datapack-language-server # Not added yet to the packages repo */
+        # SPGoding.datapack-language-server # Not added yet to the packages repo
       ];
     };
   };
