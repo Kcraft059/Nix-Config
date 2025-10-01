fd0dcf27790d5dd5d4445aa21497144d7bd26aee
Kcraft059 <kcraft059.msg@gmail.com>
2025-10-01 18:49:09 +0200

    Various updates

--- home/home-config.nix
+++ home/home-config.nix
@@ -30,7 +30,7 @@
       pkgs.speedtest-go
       pkgs.mtr
       pkgs.eza
-      pkgs.mcrcon
+      # pkgs.mcrcon
       # pkgs.devenv maybe later see https://devenv.sh - alternative to nix-shells
 
     ] ++ lib.optionals config.home-config.GUIapps.enable [
