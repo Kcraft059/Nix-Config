9b642005b07bf2ad16b5d96d1bbfc264f5dae8c5
Kcraft059 <kcraft059.msg@gmail.com>
2025-10-06 18:49:06 +0200

    Changed config for ghostty

--- home/darwin/ghostty.nix
+++ home/darwin/ghostty.nix
@@ -9,25 +9,24 @@ let
 in
 {
   # home.packages = [ pkgs.ghostty ];
-  home.file.".config/ghostty/config".text =
-    lib.optionalString config.home-config.GUIapps.enable (toKeyValue
-      { mkKeyValue = mkKeyValueDefault { } " = "; }
-      {
-        theme = "dark:rose-pine-moon,light:rose-pine-dawn";
-        background-blur = 15;
-        background-opacity = 0.85;
-        #window-step-resize = true;
-        window-height = 30;
-        window-width = 109;
-        window-padding-x = 5;
-        quit-after-last-window-closed = true;
-        #macos-icon = "custom-style";
-        #macos-icon-ghost-color = "#F6C177";
-        #macos-icon-screen-color = "#232137";
-        #keybind = global:cmd+i=change_title_prompt
-        keybind = "global:cmd+opt+ctrl+space=toggle_quick_terminal";
-        quick-terminal-position = "center";
-        auto-update = "download";
-        term = "xterm-256color";
-      });
+  home.file.".config/ghostty/config".text = lib.optionalString config.home-config.GUIapps.enable (
+    toKeyValue { mkKeyValue = mkKeyValueDefault { } " = "; } {
+      theme = "dark:Rose Pine Moon,light:Rose Pine Dawn";
+      background-blur = 15;
+      background-opacity = 0.85;
+      #window-step-resize = true;
+      window-height = 30;
+      window-width = 109;
+      window-padding-x = 5;
+      quit-after-last-window-closed = true;
+      #macos-icon = "custom-style";
+      #macos-icon-ghost-color = "#F6C177";
+      #macos-icon-screen-color = "#232137";
+      #keybind = global:cmd+i=change_title_prompt
+      keybind = "global:cmd+opt+ctrl+space=toggle_quick_terminal";
+      quick-terminal-position = "center";
+      auto-update = "download";
+      term = "xterm-256color";
+    }
+  );
 }
