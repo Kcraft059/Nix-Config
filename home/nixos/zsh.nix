84531fccdeec0a5104dac641603d0db967cfd12d
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-13 11:48:51 +0100

    Updated prompt aspect on zsh nixos

--- home/nixos/zsh.nix
+++ home/nixos/zsh.nix
@@ -11,19 +11,10 @@
     # Different of initExtra by adding First, it will be the first entry to be added
     initExtraFirst = # lib.optionalString builtins.elem "a"
       ''
-        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-        # Initialization code that may require console input (password prompts, [y/n]
-        # confirmations, etc.) must go above this block; everything else may go below.
-        #if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        #  source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-        #fi
-
-        #source /opt/homebrew /share/powerlevel10k/powerlevel10k.zsh-theme
-        #source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme
-
-        # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-
-        #[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+        # Prompt aspect
+        autoload -U colors && colors
+        NEWLINE=$'\n'
+        PS1="%{$NEWLINE%}%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[blue]%}@%{$fg[green]%}%m%{$fg[magenta]%}:%{$fg[cyan]%}%~%{$fg[red]%}]%{$reset_color%}$ "
 
         alias ll="eza --long --header --git --icons=auto"
         alias fzf-p="fzf --preview='bat --color=always --style=numbers {}' --bind 'focus:transform-header:file --brief {}'"
