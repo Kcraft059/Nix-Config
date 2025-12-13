39380de48dd75625be87fe505073eb8a07508248
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-13 12:29:52 +0100

    fixed unexpected list

--- home/atuin.nix
+++ home/atuin.nix
@@ -5,7 +5,7 @@
     enableZshIntegration = true;
     settings = {
     }
-    // lib.optionals config.home-config.external-drive.enable {
+    // lib.optionalAttrs config.home-config.external-drive.enable {
       db_path = "/Volumes/Data/camille/Apps-Data/Atuin/history.db";
     };
   };
