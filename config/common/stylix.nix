0a9cdd0f223dfa661abfe102a9cf01e6b385aa3f
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-13 12:48:23 +0100

    fixed issues

--- config/common/stylix.nix
+++ config/common/stylix.nix
@@ -7,11 +7,19 @@
 {
   options = {
     common.stylix.enable = lib.mkEnableOption ''Desktop manager wide theme'';
+    common.stylix.wallpaper = lib.mkOption {
+      type = lib.types.path;
+      default = "";
+      example = lib.literalExpression ''/ressources/wallpaper.png'';
+      description = ''
+        Set the default wallpaper
+      '';
+    };
   };
 
   config = lib.mkIf config.common.stylix.enable {
     stylix.enable = true;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
-    stylix.image = ../../ressources/Lake_Aurora.png;
+    stylix.image = config.common.stylix.wallpaper;
   };
 }
