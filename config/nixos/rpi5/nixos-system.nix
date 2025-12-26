b3a2392d69e16c8ce3fd2710b4deb2d0f40a473f
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-26 14:45:44 +0100

    why ?

--- config/nixos/rpi5/nixos-system.nix
+++ config/nixos/rpi5/nixos-system.nix
@@ -12,18 +12,5 @@
   ];
 
   config = {
-
-    ## RPI Boot loader & kernel - do not touch.
-    boot.loader.raspberryPi.bootloader = "kernel";
-
-    system.nixos.tags =
-      let
-        cfg = config.boot.loader.raspberryPi;
-      in
-      [
-        "raspberry-pi-${cfg.variant}"
-        cfg.bootloader
-        config.boot.kernelPackages.kernel.version
-      ];
   };
 }
