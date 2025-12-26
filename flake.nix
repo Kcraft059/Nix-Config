b94acaa1421e20c73b1f2c21951763ac825af0a4
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-26 14:45:44 +0100

    why ?

--- flake.nix
+++ flake.nix
@@ -496,6 +496,19 @@
                   ## Temporary
                   stylix.targets.gnome.enable = false;
                   stylix.targets.qt.enable = false;
+
+                  ## RPI Boot loader & kernel - do not touch.
+                  boot.loader.raspberryPi.bootloader = "kernel";
+
+                  system.nixos.tags =
+                    let
+                      cfg = config.boot.loader.raspberryPi;
+                    in
+                    [
+                      "raspberry-pi-${cfg.variant}"
+                      cfg.bootloader
+                      config.boot.kernelPackages.kernel.version
+                    ];
                 }
                 ## Main system config
                 ./config/nixos/regular/default.nix
