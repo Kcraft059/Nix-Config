0bcb19d25ac230bfe0df9b43b96095cb9dec0268
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-27 13:12:33 +0100

    broken

--- home/nixos/plasma.nix
+++ home/nixos/plasma.nix
@@ -9,6 +9,7 @@
     home-config.plasma.enable = lib.mkEnableOption ''Enable Plasma configuration and its plugins'';
   };
   config = lib.mkIf config.home-config.plasma.enable {
-    home.file.".gtkrc-2.0".force = true;
+    stylix.targets.gtk.enable = false;
+    #home.file.".gtkrc-2.0".force = lib.mkForce true;
   };
 }
