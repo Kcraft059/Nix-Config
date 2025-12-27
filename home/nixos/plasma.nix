37f1b8ca74fc246982493cd65f24d7fbeb5af8e7
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-27 21:48:50 +0100

    idk

--- home/nixos/plasma.nix
+++ home/nixos/plasma.nix
@@ -9,5 +9,6 @@
     home-config.plasma.enable = lib.mkEnableOption ''Enable Plasma configuration and its plugins'';
   };
   config = lib.mkIf config.home-config.plasma.enable {
+    stylix.targets.gtk.enable = false;
   };
 }
