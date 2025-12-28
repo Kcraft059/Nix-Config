b4ef281d7d3095eb55dd014315ecb5faa53c57f9
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-28 13:25:15 +0100

    minor additions

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -10,7 +10,7 @@
     programs.alacritty = {
       enable = config.home-config.GUIapps.enable;
       settings = {
-        font.size = 12;
+        font.size = 11;
         font.normal = {
           family = "JetBrainsMono Nerd Font";
         };
@@ -20,6 +20,7 @@
             y = 0;
           };
           opacity = 0.85;
+          blur = true;
         };
         general = {
           import = [ "${pkgs.alacritty-theme}/share/alacritty-theme/rose_pine_moon.toml" ];
