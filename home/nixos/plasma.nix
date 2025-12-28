cc45bb44509e91b571e16a8125faea4ae1bd2000
Kcraft059 <kcraft059.msg@gmail.com>
2025-12-28 13:25:29 +0100

    plasma-manager addition

--- home/nixos/plasma.nix
+++ home/nixos/plasma.nix
@@ -5,10 +5,17 @@
   ...
 }:
 {
-  options = { 
+  options = {
     home-config.plasma.enable = lib.mkEnableOption ''Enable Plasma configuration and its plugins'';
   };
+
   config = lib.mkIf config.home-config.plasma.enable {
+    # Stylix overrides
     stylix.targets.gtk.enable = false;
+
+    # Plasma configuration
+    programs.plasma = {
+      enable = true;
+    };
   };
 }
