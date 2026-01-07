34a0ecaae289a90b3ede75018128a104169239a2
Kcraft059 <kcraft059.msg@gmail.com>
2026-01-07 13:03:53 +0100

    rpi-keyboard-config

---
+++ overlays/rpi-keyboard-config.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "rpi-keyboard-config";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "raspberrypi";
+    repo = "rpi-keyboard-config";
+    rev = "v${version}"; # or a commit hash
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  format = "setuptools";
+
+  # Runtime dependencies (adjust after checking imports)
+  propagatedBuildInputs = with python3Packages; [
+    click
+    hidapi
+  ];
+
+  # No tests upstream
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "RPiKeyboardConfig"
+  ];
+
+  meta = with lib; {
+    description = "RPi Keyboard configuration utility";
+    homepage = "https://github.com/raspberrypi/rpi-keyboard-config";
+    license = licenses.bsd3;
+    maintainers = [ ];
+    platforms = platforms.linux;
+  };
+}
