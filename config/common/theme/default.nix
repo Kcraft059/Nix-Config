2f508a07f27d8c01ea3b6e02d30879f4cbc5bbe7
Kcraft059 <kcraft059.msg@gmail.com>
2026-03-14 00:12:51 +0100

    Further preparation work

--- config/common/theme/default.nix
+++ config/common/theme/default.nix
@@ -7,30 +7,42 @@ in
     ./stylix.nix
   ];
 
-  /* options.common.theme = {
-    wallpaper = mkOption {
-      type = types.path;
-      default = "";
-      example = lib.literalExpression ''./ressources/wallpaper.png'';
-      description = "Wallpaper";
-    };
-    colors = mkOption {
-      type = types.attrs;
-      default = { };
-      description = "Global color palette";
-    };
-  };
+  /*
+    # Feature list:
+    # Colors (including accent), name, theme (dark/light), wallpaper, vs-extension
 
-  options.darwin.theme = {
-    enclosureNumber = mkOption {
-      type = types.int;
-      default = 0;
-      description = "Set the macos enclosureNumber (0 will act as not set)";
+    options.common.theme = {
+      wallpaper = mkOption {
+        type = types.path;
+        default = "";
+        example = lib.literalExpression "./ressources/wallpaper.png";
+        description = "Wallpaper";
+      };
+      colors = mkOption {
+        type = types.attrs;
+        default = { };
+        description = "Global color palette";
+      };
     };
-  };
 
-  config = {
-    stylix.enable = mkDefault true;
-    stylix.image = mkDefault config.common.theme.wallpaper;
-  }; */
+    options.common.theme.utils = mkOption {
+      default = {
+        helper = var: "print: ${var}";
+      };
+    };
+
+    options.darwin.theme = {
+      enclosureNumber = mkOption {
+        type = types.int;
+        default = 0;
+        description = "Set the macos enclosureNumber (0 will act as not set)";
+      };
+    };
+
+    config = {
+      stylix.enable = mkDefault true;
+      stylix.image = mkDefault config.common.theme.wallpaper;
+    };
+  */
+
 }
